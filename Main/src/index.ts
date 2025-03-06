c02038e52dbeb3766b07448548d4f1f55eae5508
gamalmubarak <eng-gemy@hotmail.com>
2025-03-06 13:35:48 -0500

    Main prompt function updated

--- Main/src/index.ts
+++ Main/src/index.ts
@@ -26,3 +26,56 @@ export const mainPrompt = async () => {
         ],
       },
     ]);
+
+    switch (answers.mainMenu) {
+        case 'View all departments':
+          await viewAllDepartments();
+          break;
+        case 'View all roles':
+          await viewAllRoles();
+          break;
+        case 'View all employees':
+          await viewAllEmployees();
+          break;
+        case 'Add a department':
+          await addDepartment();
+          break;
+        case 'Add a role':
+          await addRole();
+          break;
+        case 'Add an employee':
+          await addEmployee();
+          break;
+        case 'Update an employee role':
+          await updateEmployeeRole();
+          break;
+        case 'Update an employee manager':
+          await updateEmployeeManager();
+          break;
+        case 'View employees by manager':
+          await viewEmployeesByManager();
+          break;
+        case 'View employees by department':
+          await viewEmployeesByDepartment();
+          break;
+        case 'Delete a department':
+          await deleteDepartment();
+          break;
+        case 'Delete a role':
+          await deleteRole();
+          break;
+        case 'Delete an employee':
+          await deleteEmployee();
+          break;
+        case 'View total utilized budget of a department':
+          await viewDepartmentBudget();
+          break;
+        case 'Quit':
+          console.log('Goodbye!');
+          process.exit();
+      }
+    
+      mainPrompt();
+    };
+    
+    mainPrompt();
