867ddd5f8e20c79f4a2525f8b06bde102a6bde53
gamalmubarak <eng-gemy@hotmail.com>
2025-03-06 13:13:39 -0500

    Function to view all roles added

--- Main/src/db/queries.ts
+++ Main/src/db/queries.ts
@@ -7,3 +7,8 @@ export const viewAllDepartments = async () => {
   const result = await pool.query('SELECT id, name FROM department');
   console.table(result.rows);
 };
+// Function to view all roles
+export const viewAllRoles = async () => {
+    const result = await pool.query('SELECT r.id, r.title, r.salary, d.name AS department FROM role r JOIN department d ON r.department_id = d.id');
+    console.table(result.rows);
+  };
