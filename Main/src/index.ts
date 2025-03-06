247a0a8803d6aa08d903fe8e0332ad4c6951d4cb
gamalmubarak <eng-gemy@hotmail.com>
2025-03-06 13:10:23 -0500

    Main prompt function added

---
+++ Main/src/index.ts
@@ -0,0 +1,28 @@
+import inquirer from 'inquirer';
+
+// Main prompt function
+export const mainPrompt = async () => {
+    const answers = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'mainMenu',
+        message: 'What would you like to do?',
+        choices: [
+          'View all departments',
+          'View all roles',
+          'View all employees',
+          'Add a department',
+          'Add a role',
+          'Add an employee',
+          'Update an employee role',
+          'Update an employee manager',
+          'View employees by manager',
+          'View employees by department',
+          'Delete a department',
+          'Delete a role',
+          'Delete an employee',
+          'View total utilized budget of a department',
+          'Quit',
+        ],
+      },
+    ]);
